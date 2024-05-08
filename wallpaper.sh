0e7f6c4b170c33694df973c3be769a3fa394606f
Radosław Kłak <radek.klak@gmail.com>
2024-05-09 01:03:08 +0200

    feat(pictures): rename to polish dir

--- wallpaper.sh
+++ wallpaper.sh
@@ -8,7 +8,7 @@ night_start=20
 night_end=6
 
 if [ "$current_hour" -ge $night_start ] || [ "$current_hour" -lt $night_end ]; then
-	feh --bg-fill ~/Pictures/Wallpaper/night.jpg
+	feh --bg-fill ~/Obrazy/Wallpaper/night.jpg
 else
-	feh --bg-fill ~/Pictures/Wallpaper/iceland.jpg
+	feh --bg-fill ~/Obrazy/Wallpaper/iceland.jpg
 fi
