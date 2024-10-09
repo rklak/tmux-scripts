efbb369722b7a606bdf41a90f6a960b8815644ec
Radosław Kłak <radoslaw.klak@solidstudio.io>
2024-10-10 00:44:55 +0200

    feat(develop): develop not exists case

--- lib/colors.sh
+++ lib/colors.sh
@@ -1,50 +1,50 @@
 #!/usr/bin/env bash
 
-if ! declare -F echo_success &>/dev/null; then
+# if ! declare -F echo_success &>/dev/null; then
 
-	# Colors
-	error=$(tput setaf 160)
-	info=$(tput setaf 44)
-	warn=$(tput setaf 214)
-	success=$(tput setaf 34)
-	bold=$(tput bold)
+# Colors
+error=$(tput setaf 160)
+info=$(tput setaf 44)
+warn=$(tput setaf 214)
+success=$(tput setaf 34)
+bold=$(tput bold)
 
-	# Reset
-	reset=$(tput sgr0)
+# Reset
+reset=$(tput sgr0)
 
-	echo_info() {
-		local newline_arg="" # Initialize a variable to store the newline argument
-		if [[ $1 == "-n" ]]; then
-			newline_arg="-n" # Set newline_arg to "-n" if the first argument is "-n"
-			shift            # Shift the arguments to remove the first argument ("-n")
-		fi
-		echo $newline_arg "${info}$@${reset}"
-	}
+echo_info() {
+  local newline_arg="" # Initialize a variable to store the newline argument
+  if [[ $1 == "-n" ]]; then
+    newline_arg="-n" # Set newline_arg to "-n" if the first argument is "-n"
+    shift            # Shift the arguments to remove the first argument ("-n")
+  fi
+  echo $newline_arg "${info}$@${reset}"
+}
 
-	echo_warn() {
-		local newline_arg="" # Initialize a variable to store the newline argument
-		if [[ $1 == "-n" ]]; then
-			newline_arg="-n" # Set newline_arg to "-n" if the first argument is "-n"
-			shift            # Shift the arguments to remove the first argument ("-n")
-		fi
-		echo $newline_arg "${warn}$@${reset}"
-	}
+echo_warn() {
+  local newline_arg="" # Initialize a variable to store the newline argument
+  if [[ $1 == "-n" ]]; then
+    newline_arg="-n" # Set newline_arg to "-n" if the first argument is "-n"
+    shift            # Shift the arguments to remove the first argument ("-n")
+  fi
+  echo $newline_arg "${warn}$@${reset}"
+}
 
-	echo_error() {
-		local newline_arg="" # Initialize a variable to store the newline argument
-		if [[ $1 == "-n" ]]; then
-			newline_arg="-n" # Set newline_arg to "-n" if the first argument is "-n"
-			shift            # Shift the arguments to remove the first argument ("-n")
-		fi
-		echo $newline_arg "${bold}${error}$@${reset}"
-	}
+echo_error() {
+  local newline_arg="" # Initialize a variable to store the newline argument
+  if [[ $1 == "-n" ]]; then
+    newline_arg="-n" # Set newline_arg to "-n" if the first argument is "-n"
+    shift            # Shift the arguments to remove the first argument ("-n")
+  fi
+  echo $newline_arg "${bold}${error}$@${reset}"
+}
 
-	echo_success() {
-		local newline_arg="" # Initialize a variable to store the newline argument
-		if [[ $1 == "-n" ]]; then
-			newline_arg="-n" # Set newline_arg to "-n" if the first argument is "-n"
-			shift            # Shift the arguments to remove the first argument ("-n")
-		fi
-		echo $newline_arg "${success}$@${reset}"
-	}
-fi
+echo_success() {
+  local newline_arg="" # Initialize a variable to store the newline argument
+  if [[ $1 == "-n" ]]; then
+    newline_arg="-n" # Set newline_arg to "-n" if the first argument is "-n"
+    shift            # Shift the arguments to remove the first argument ("-n")
+  fi
+  echo $newline_arg "${success}$@${reset}"
+}
+# fi
--- worktree/gwta.sh
+++ worktree/gwta.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
+source "$HOME/scripts/lib/colors.sh"
+
 # Check if the argument is provided
 if [ -z "$1" ]; then
   echo_info "Usage: $0 <branch_name>"
   exit 1
 fi
-
-success=$(tput setaf 34)
-reset=$(tput sgr0)
 
 # Capture the original string from the command-line argument
 branch_name="$1"
@@ -15,14 +14,19 @@ branch_name="$1"
 # Replace '/' with '-'
 branch_directory=${branch_name//\//-}
 
-echo "Checking branch at remote"
-
 if git worktree add "$branch_directory" "$branch_name" >/dev/null 2>&1; then
-  echo "Yeah, so from remote…"
+  echo "📡 Found on a remote"
 else
-  echo "Nope, so from local develop"
-  git worktree add -b "$branch_name" "$branch_directory" origin/develop
+  branch_main=""
+  if [ $(git branch --list develop) ]; then
+    branch_main="develop"
+  else
+    branch_main="master"
+  fi
+
+  echo "🚀 Creating from origin/$branch_main"
+  git worktree add -b "$branch_name" "$branch_directory" origin/"$branch_main" >/dev/null 2>&1
 fi
 
-cd "$branch_directory" || (echo_error "Directory branch doesn't exists: ${branch_directory}" && exit 1)
-echo "${success}Wortkree created and you are in it${reset}"
+cd "$branch_directory" || (echo_error "💥 Directory branch doesn't exists: ${branch_directory}" && exit 1)
+echo_success "🚧 Wortkree created and you are in it"
